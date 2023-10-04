d30151f5ed89502eb454082151705c576fa3b2d0
wltjd1688 <wltjd1688@gmail.com>
2023-10-04 13:24:59 +0000

    [error]kernel panic 해결

--- do.sh
+++ do.sh
@@ -1,8 +1,10 @@
-cd threads
+cd userprog
 make clean
 make
 cd build
 source ../../activate
-pintos -- -q run priority-donate-chain
+# pintos -- -q run priority-donate-chain
 # pintos -- -q run alarm-simultaneous
 # pintos --gdb -- run priority-donate-nest
+
+pintos --fs-disk=10 -p tests/userprog/args-single:args-single -- -q -f run 'args-single onearg'
