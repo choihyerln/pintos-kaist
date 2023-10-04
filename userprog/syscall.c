d4c0641f3179702b7e1cd2498ceb836a0936d446
choihyerln <hr_1227@naver.com>
2023-10-04 12:04:33 +0000

    systemcall.c 한글주석달기

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -11,36 +11,37 @@
 void syscall_entry (void);
 void syscall_handler (struct intr_frame *);
 
-/* System call.
+/* 시스템 호출.
  *
- * Previously system call services was handled by the interrupt handler
- * (e.g. int 0x80 in linux). However, in x86-64, the manufacturer supplies
- * efficient path for requesting the system call, the `syscall` instruction.
+ * 이전에 시스템 호출 서비스는 인터럽트 핸들러에 의해 처리되었습니다
+ * (예: 리눅스에서 int 0x80). 그러나 x86-64에서 제조업체는
+ * 시스템 호출을 요청하기 위한 효율적인 경로를 제공합니다. 이 경로는
+ * `syscall` 명령어로 작동합니다.
  *
- * The syscall instruction works by reading the values from the the Model
- * Specific Register (MSR). For the details, see the manual. */
+ * syscall 명령은 Model Specific Register (MSR)에서 값을 읽어오는 방식으로 동작합니다.
+ * 자세한 내용은 매뉴얼을 참조하십시오. */
 
-#define MSR_STAR 0xc0000081         /* Segment selector msr */
-#define MSR_LSTAR 0xc0000082        /* Long mode SYSCALL target */
-#define MSR_SYSCALL_MASK 0xc0000084 /* Mask for the eflags */
+#define MSR_STAR 0xc0000081         /* 세그먼트 선택자 MSR */
+#define MSR_LSTAR 0xc0000082        /* 롱 모드 SYSCALL 대상 */
+#define MSR_SYSCALL_MASK 0xc0000084 /* EFLAGS에 대한 마스크 */
 
 void
 syscall_init (void) {
-	write_msr(MSR_STAR, ((uint64_t)SEL_UCSEG - 0x10) << 48  |
-			((uint64_t)SEL_KCSEG) << 32);
-	write_msr(MSR_LSTAR, (uint64_t) syscall_entry);
+    write_msr(MSR_STAR, ((uint64_t)SEL_UCSEG - 0x10) << 48  |
+            ((uint64_t)SEL_KCSEG) << 32);
+    write_msr(MSR_LSTAR, (uint64_t) syscall_entry);
 
-	/* The interrupt service rountine should not serve any interrupts
-	 * until the syscall_entry swaps the userland stack to the kernel
-	 * mode stack. Therefore, we masked the FLAG_FL. */
-	write_msr(MSR_SYSCALL_MASK,
-			FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
+    /* 시스템 호출 진입점이 사용자 영역 스택을 커널 모드 스택으로 교체할 때까지
+        인터럽트 서비스 루틴은 어떠한 인터럽트도 처리해서는 안 됩니다.
+        따라서 FLAG_FL을 마스킹했습니다. */
+    write_msr(MSR_SYSCALL_MASK,
+            FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
 }
 
-/* The main system call interface */
+/* 주요 시스템 호출 인터페이스 */
 void
 syscall_handler (struct intr_frame *f UNUSED) {
-	// TODO: Your implementation goes here.
-	printf ("system call!\n");
-	thread_exit ();
+    // TODO: 여기에 구현이 들어갑니다.
+    printf ("system call!\n");
+    thread_exit ();
 }
