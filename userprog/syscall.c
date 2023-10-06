e20b5e17275eed640960151b0c7c60e231237f55
hjlee5701 <hjlee5701@gamil.com>
2023-10-06 06:13:36 +0000

    syscall_handler switch문 및 함수 초기화

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -7,7 +7,7 @@
 #include "userprog/gdt.h"
 #include "threads/flags.h"
 #include "intrinsic.h"
-
+#include "threads/init.h"
 void syscall_entry (void);
 void syscall_handler (struct intr_frame *);
 
@@ -37,14 +37,95 @@ syscall_init (void) {
     write_msr(MSR_SYSCALL_MASK,
             FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
 }
+void halt (void){
+    power_off();
+}
+void exit (int status){
 
+}
+pid_t fork (const char *thread_name){
+
+}
+int exec (const char *cmd_line){
+
+}
+int wait (pid_t pid){
+
+}
+bool create (const char *file, unsigned initial_size){
+
+}
+bool remove (const char *file){
+
+}
+bool remove (const char *file){
+
+}
+int filesize (int fd){
+
+}
+int filesize (int fd){
+
+}
+int write (int fd, const void *buffer, unsigned size){
+
+}
+void seek (int fd, unsigned position){
+
+}
+unsigned tell (int fd){
+
+}
+void close (int fd){
+    
+}
 /* 주요 시스템 호출 인터페이스 */
 void
 syscall_handler (struct intr_frame *f UNUSED) {
-    if (f->R.rax == SYS_WRITE) {
+    switch(f->R.rax){
+        case SYS_HALT:
+            void halt(void); break;
+        
+        case SYS_EXIT:
+            thread_exit(); break;
+        
+        case SYS_FORK:
+            break;
+        
+        case SYS_EXEC:
+            break;
+        
+        case SYS_WAIT:
+            break;
+        
+        case SYS_CREATE:
+            break;
+        
+        case SYS_REMOVE:
+            break;
+        
+        case SYS_OPEN:
+            break;
+        
+        case SYS_FILESIZE:
+            break;
+        
+        case SYS_READ:
+            break;
+        
+        case SYS_WRITE:
+            printf("%s", f->R.rsi); break;
+        
+        case SYS_SEEK:
+            break;
+        
+        case SYS_TELL:
+            break;
+        
+        case SYS_CLOSE:
+            break;
 
-        printf("%s", f->R.rsi);
+        default:
+            break;                                                                                                      
     }
-    if(f->R.rax == SYS_EXIT)
-        thread_exit ();
 }
