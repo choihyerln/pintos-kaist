70b2a8499319b527f71bfdbc2cae6455135d1a0b
hjlee5701 <hjlee5701@gamil.com>
2023-10-09 19:22:20 +0000

    system call - write 구현

--- do.sh
+++ do.sh
@@ -1,13 +1,16 @@
-cd threads
+cd userprog
 make clean
 make
 cd build
 source ../../activate
 pintos-mkdisk filesys.dsk 10 
 # make check
+# pintos --fs-disk=10 -p tests/userprog/write-twice:write-twice -p ../../tests/userprog/sample.txt:sample.txt -- -q -f run 'write-twice'
+# pintos --fs-disk=10 -p tests/userprog/write-twice:write-twice -p ../../tests/userprog/sample.txt:sample.txt -- -q -f run write-twice
+pintos --fs-disk=10 -p tests/userprog/write-stdin:write-stdin -- -q   -f run write-stdin
 
-# pintos --fs-disk=10 -p tests/userprog/fork-read:fork-read -p ../../tests/userprog/sample.txt:sample.txt -- -q -f run 'fork-read'
+# pintos --fs-disk=10 -p tests/userprog/write-normal:write-normal -p ../../tests/userprog/sample.txt:sample.txt -- -q -f run 'write-normal'
 # pintos --fs-disk=10 -p tests/userprog/read-normal:read-normal -p ../../tests/userprog/sample.txt:sample.txt -- -q   -f run read-normal
 # pintos --fs-disk=10 -p tests/userprog/fork-once:fork-once -- -q -f run 'fork-once'
 # pintos tests/userprog/fork-once -- -q -f run
-pintos -- -q run priority-change
+# pintos -- -q run priority-change
